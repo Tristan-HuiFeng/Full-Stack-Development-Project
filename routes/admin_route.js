877b4d9c549e17a59f28cd62ec9725e96e7ca19f
Hieu Nguyen <48639956+schwahue@users.noreply.github.com>
2020-05-19 22:41:18 +0800

    adminUpdate
    1. added in html routing elements
    2. added additional routing methods
    3. updated miscelaneous items

--- routes/admin_route.js
+++ routes/admin_route.js
@@ -2,16 +2,26 @@ const express = require('express');
 const router = express.Router();
 const alertMessage = require('../helpers/messenger.js');
 
+//dev log
+//added routing to respective webstie
+//using dummy data for demonstrations
+
 let admin = true //display admin features
 
-let storeCategories = [{'category':'tech lead'}, {'category': 'not tech lead'}]
+let storeCategories = [{'category':'tech lead'}, {'category': 'not tech lead'}] //dummy data
+
+let inventory = [{'itemID':'1','itemName':'Tristan','itemCost':'FOC', 'itemStock':'69'},
+{'itemID':'2','itemName':'Jun Hui','itemCost':'FOC', 'itemStock':'69'},
+{'itemID':'3','itemName':'Hieu','itemCost':'FOC', 'itemStock':'69'},
+{'itemID':'4','itemName':'Matthew','itemCost':'FOC', 'itemStock':'69'}] //dummy data
 
 router.get('/', (req,res) => {
-    res.render('index', {admin})
+    res.render('index', {admin, inventory})
 });
 
 router.get('/addProduct', (req,res) =>{
     res.render('product/addProduct', {admin, storeCategories})
 });
+
 
 module.exports = router;
