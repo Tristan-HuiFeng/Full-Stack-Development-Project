fa1060ce5508c5e3a8923254972ea3df9d0c98ae
schwahue <48639956+schwahue@users.noreply.github.com>
2020-05-19 19:33:23 +0800

    addProduct - page
    1. add add product page
    2. add routing to addProductPage

--- routes/admin_route.js
+++ routes/admin_route.js
@@ -2,8 +2,16 @@ const express = require('express');
 const router = express.Router();
 const alertMessage = require('../helpers/messenger.js');
 
+let admin = true //display admin features
+
+let storeCategories = [{'category':'tech lead'}, {'category': 'not tech lead'}]
+
 router.get('/', (req,res) => {
-    res.render('index', {admin : true})
-})
+    res.render('index', {admin})
+});
+
+router.get('/addProduct', (req,res) =>{
+    res.render('product/addProduct', {admin, storeCategories})
+});
 
 module.exports = router;
